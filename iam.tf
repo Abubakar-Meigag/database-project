a0f63c815077529a0b5c62c8f7f7462a604edaf8
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2025-02-03 18:34:37 +0000

    update fix error

--- iam.tf
+++ iam.tf
@@ -50,29 +50,4 @@ resource "aws_iam_instance_profile" "ec2_instance_profile" {
   role = aws_iam_role.ec2_ssm_role.name
 }
 
-resource "aws_iam_policy" "ssm_policy" {
-  name        = "EC2SSMParameterStorePolicy"
-  description = "Policy to allow EC2 to retrieve SSM parameters"
-  
-  policy = jsonencode({
-    Version = "2012-10-17" # ✅ Add this line to fix the error
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "ssm:GetParameter",
-          "ssm:GetParameters",
-          "ssm:GetParametersByPath"
-        ]
-        Resource = "arn:aws:ssm:eu-west-2:YOUR_AWS_ACCOUNT_ID:parameter/myapp/*"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "kms:Decrypt"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
+
