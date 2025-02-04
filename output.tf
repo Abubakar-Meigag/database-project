4cf905dc1c2d2f75146855f217db99db36d462a4
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2025-02-04 19:30:43 +0000

    update fix error

--- output.tf
+++ output.tf
@@ -3,17 +3,17 @@ output "ec2_instance_id" {
 }
 
 output "subnet_id" {
-  value = aws_subnet.private_subnet.id
+  value = aws_subnet.database-vpc-private-subnet.id
 }
 
 output "security_group_id" {
-  value = aws_security_group.ec2_sg.id
+  value = aws_security_group.database-vpc-ec2-sg.id
 }
 
 output "public_subnet_id" {
-  value = aws_subnet.public_subnet.id
+  value = aws_subnet.database-vpc-public-subnet.id
 }
 
 output "internet_gateway_id" {
-  value = aws_internet_gateway.igw.id
+  value = aws_internet_gateway.database-vpc-igw.id
 }
