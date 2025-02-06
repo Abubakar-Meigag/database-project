a787aec9d25b7ae7139add25d93859d7a4a8c474
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2025-02-06 18:14:23 +0000

    update

--- iam.tf
+++ iam.tf
@@ -67,8 +67,3 @@ resource "aws_iam_role_policy_attachment" "ssm_attach" {
   role       = aws_iam_role.ec2_ssm_role.name
   policy_arn = aws_iam_policy.ssm_policy.arn
 }
-
-resource "aws_iam_instance_profile" "ec2_instance_profile" {
-  name = "EC2InstanceProfile"
-  role = aws_iam_role.ec2_ssm_role.name
-}
