9cc4d2e4ed8efbccaa4fa4d0fe5e101f11923bae
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2025-02-08 16:28:19 +0000

    update

--- app-install.sh
+++ app-install.sh
@@ -5,3 +5,5 @@ sudo yum install -y httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
 sudo echo '<center><h1>Welcome to New Stack to build and host database into EC2 instance!</h1></center>' > /var/www/html/index.html
+
+Password@19
