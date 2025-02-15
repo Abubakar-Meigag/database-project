a821aa3380d31c264008254e7b15202f914e9ba5
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2025-02-15 21:51:08 +0000

    update

--- lambda_terraform.tf
+++ lambda_terraform.tf
@@ -80,19 +80,19 @@ resource "aws_cloudwatch_event_target" "stop_target" {
 }
 
 # Allow EventBridge to Invoke Lambda
-# resource "aws_lambda_permission" "allow_eventbridge_start" {
-#   statement_id  = "AllowExecutionFromEventBridgeStart"
-#   action        = "lambda:InvokeFunction"
-#   function_name = aws_lambda_function.ec2_scheduler.function_name
-#   principal     = "events.amazonaws.com"
-#   source_arn    = aws_cloudwatch_event_rule.ec2_start_schedule.arn
-# }
+resource "aws_lambda_permission" "allow_eventbridge_start" {
+  statement_id  = "AllowExecutionFromEventBridgeStart"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.ec2_scheduler.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.ec2_start_schedule.arn
+}
 
-# resource "aws_lambda_permission" "allow_eventbridge_stop" {
-#   statement_id  = "AllowExecutionFromEventBridgeStop"
-#   action        = "lambda:InvokeFunction"
-#   function_name = aws_lambda_function.ec2_scheduler.function_name
-#   principal     = "events.amazonaws.com"
-#   source_arn    = aws_cloudwatch_event_rule.ec2_stop_schedule.arn
-# }
+resource "aws_lambda_permission" "allow_eventbridge_stop" {
+  statement_id  = "AllowExecutionFromEventBridgeStop"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.ec2_scheduler.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.ec2_stop_schedule.arn
+}
 
