66a8ab151da9284a32b01fe891ec60c46f571246
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2025-02-15 22:34:24 +0000

    update

--- lambda_terraform.tf
+++ lambda_terraform.tf
@@ -60,7 +60,7 @@ resource "aws_cloudwatch_event_rule" "ec2_start_schedule" {
 
 resource "aws_cloudwatch_event_rule" "ec2_stop_schedule" {
   name                = "ec2-stop-schedule"
-  schedule_expression = "cron(59 11 * * ? *)"
+  schedule_expression = "cron(30 11 * * ? *)"
 }
 
 resource "aws_cloudwatch_event_target" "start_target" {
